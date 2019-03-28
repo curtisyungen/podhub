a97e854764e3a0fed2b4d783ee5dbc6ce1c8e5f1
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-03-28 11:49:09 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
   resetState = () => {
     this.setState({
       podcastSearch: ""
-    }, () => { this.checkContent })
+    }, () => this.checkContent )
   }
 
   render() {
