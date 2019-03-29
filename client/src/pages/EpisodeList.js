545613c9b219f875507111528b632f0ea07fb99e
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-03-29 09:42:29 -0700

    Add files via upload

--- client/src/pages/EpisodeList.js
+++ client/src/pages/EpisodeList.js
@@ -29,7 +29,13 @@ class EpisodeList extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log(this.props);
+        if (prevProps.podcastId !== this.props.location.state.podcastId) {
+            this.setState({
+                podcastId: this.props.location.state.podcastId,
+                podcastName: this.props.location.state.podcastName,
+                podcastLogo: this.props.location.state.podcastLogo
+            }, () => {this.getEpisodes() });
+        }
     }
 
     // Get episodes for podcast by Podcast ID
