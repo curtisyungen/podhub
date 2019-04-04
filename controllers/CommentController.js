3e8410e51785c6c6bcc5286b9a41f36e80638169
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-03 22:53:15 -0700

    Add files via upload

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -9,10 +9,70 @@ class CommentController {
    * @param {*} req
    * @param {*} res
    */
-  create(req, res) {
+  createComment(req, res) {
     db.comment.findOrCreate({ where: req.body }).then(comment => res.json(comment));
   }
 
+  /**
+   * create likes for comments in database
+   * @param {*} req
+   * @param {*} res
+   */
+  createCommentLikes(req, res) {
+    db.commentLike.findOrCreate({ where: req.body }).then(commentLike => res.json(commentLike));
+  }
+
+
+  /**
+   * get users who commented post and the post likes.
+   * @param {*} req
+   * @param {*} res
+   */
+  getCommentedUser(req, res){
+    var postPromises =[];
+    var commentLikes = {};
+    var commentuserName = {};
+    var commentuserImage = {};
+    db.post.findByPk(
+      req.params.id 
+  ).then(function(dbUser){
+      dbUser.getComments().then(function (comments) {
+        const sortedComments = comments.sort(function(a, b) {
+          if (a.updatedAt < b.updatedAt) return -1;
+          if (a.updatedAt > b.updatedAt) return 1;
+          return 0;
+        });
+        sortedComments.forEach(comment => {
+          const userDetailsPromise = db.user.findByPk(comment.commentedBy);
+          const commentLikePromise = comment.getCommentLikes();
+          postPromises.push(userDetailsPromise);
+          postPromises.push(commentLikePromise);
+          
+          userDetailsPromise.then(function(user){
+            commentuserName[comment.id] = user.name;
+            commentuserImage[comment.id] = user.profileImage;
+          })
+          commentLikePromise.then(function(likes) {
+            commentLikes[comment.id] = likes.length;
+          });
+        });
+
+        Promise.all(postPromises).then(function() {
+          sortedComments.forEach(comment => {
+            comment.dataValues.numberOfLikes = commentLikes[comment.id];
+            comment.dataValues.userName = commentuserName[comment.id];
+            comment.dataValues.userImage = commentuserImage[comment.id];
+          });
+          console.log(sortedComments)
+          res.json(sortedComments);
+        })
+      }).catch(function(error) {
+        res.status(400);
+      })
+
+    })
+  }
+ 
   /**
    * Update the comment in database
    * @param {*} req
@@ -29,9 +89,19 @@ class CommentController {
    * @param {*} req
    * @param {*} res
    */
-  remove(req, res) {
-    db.comment.destroy({ where: req.params }).then(comment => res.json(comment));
+  removeComment(req, res) {
+    db.comment.destroy({ where: req.params }).then(dbcomment => res.json(dbcomment));
   }
+
+  /**
+   * Remove a comment likes in database
+   * @param {*} req
+   * @param {*} res
+   */
+  removeCommentLikes(req, res) {
+    db.commentLike.destroy({ where: req.params }).then(dbcommentLikes => res.json(dbcommentLikes));
+  }
+  
 
   /**
    * get comment likes
--- routes/api/commentRoute.js
+++ routes/api/commentRoute.js
@@ -6,8 +6,22 @@ const controller = new CommentController();
  * Route to create a new comment in database
  * @param {*} req
  * @param {*} res
+*/
+router.post("/", (req, res) => controller.createComment(req, res));
+
+/**
+ * Route to create likes for comment in database
+ * @param {*} req
+ * @param {*} res
  */
-router.post("/", (req, res) => controller.create(req, res));
+router.post("/commentLikes", (req, res) => controller.createCommentLikes(req, res));
+
+/**
+ * Get users who commented post
+ * @param {*} req
+ * @param {*} res
+ */
+router.get("/commentedUserLikes/:id", (req, res) => controller.getCommentedUser(req, res));
 
 /**
  * Route to update the existing comment
@@ -21,7 +35,14 @@ router.put("/:id", (req, res) => controller.update(req, res));
  * @param {*} req
  * @param {*} res
  */
-router.delete("/:id", (req, res) => controller.remove(req, res));
+router.delete("/:id", (req, res) => controller.removeComment(req, res));
+
+/**
+ * Route to remove the likes from comments
+ * @param {*} req
+ * @param {*} res
+ */
+router.delete("/commentUnlikes/:id", (req, res) => controller.removeCommentLikes(req, res));
 
 /**
  * Get Comment likes
