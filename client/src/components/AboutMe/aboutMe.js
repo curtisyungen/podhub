c0792acf96532fbecdca8126931ebd127d336e5d
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-10 09:23:17 -0700

    Add files via upload

--- client/src/components/AboutMe/aboutMe.js
+++ client/src/components/AboutMe/aboutMe.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Container from "../components/Container/container";
+import Container from "../Container/container";
 import API from "../../utils/API";
 import "./aboutMe.css";
 
