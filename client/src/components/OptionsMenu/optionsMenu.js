f5701ab7e96263a764959eabb91cb32a0da49525
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-11 13:42:15 -0700

    Add files via upload

--- client/src/components/OptionsMenu/optionsMenu.js
+++ client/src/components/OptionsMenu/optionsMenu.js
@@ -88,6 +88,7 @@ class OptionsMenu extends Component {
                                 user: this.state.user
                             }
                         }}
+                        hideOptionsMenu={this.hideOptionsMenu}
                     />
                 ) : (
                     <></>
