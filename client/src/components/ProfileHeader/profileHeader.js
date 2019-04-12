b36ea0067c0a7a0f0c7975fe95aa4aa277a00df5
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-12 12:26:12 -0700

    Add files via upload

--- client/src/components/ProfileHeader/profileHeader.js
+++ client/src/components/ProfileHeader/profileHeader.js
@@ -309,17 +309,16 @@ class ProfileHeader extends Component {
 
                                     <div id="editButtons">
                                         <button
-                                            className="btn btn-light btn-sm saveBtn"
-                                            onClick={this.saveProfile}
-                                        >
-                                            Save
-                                        </button>
-
-                                        <button
                                             className="btn btn-light btn-sm cancelBtn"
                                             onClick={this.cancelEditProfile}
                                         >
                                             Cancel
+                                        </button>
+                                        <button
+                                            className="btn btn-light btn-sm saveBtn"
+                                            onClick={this.saveProfile}
+                                        >
+                                            Save
                                         </button>
                                     </div>
 
