5d9152b440c346fdb52672f344712b7ebf0bd014
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-16 17:45:26 -0700

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    username: "root",
+    password: "mysqlpassword",
     database: "podHub",
     host: process.env.DB_HOST,
     dialect: "mysql",
