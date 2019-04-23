171db19364f12b6edd185431cff5499d53af76bf
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-23 10:16:28 -0700

    Add files via upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -13,7 +13,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { chevronRight } from '@fortawesome/free-solid-svg-icons'
 
-library.add(chevonRight);
+library.add(chevronRight);
 
 // USER PROFILE PAGE
 
