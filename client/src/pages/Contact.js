6890b10f02c130d19ec33367360e73be3e44d2ee
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-03 13:26:31 -0700

    Add files via upload

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Container from "../components/Container/container";
 import API from "../utils/API";
+import "./Contact.css";
 
 class Contact extends Component {
 
@@ -14,16 +15,24 @@ class Contact extends Component {
 
     componentDidMount = () => {
         this.setState({
-            user: this.props.location.state.user,
-            theme: this.props.location.state.theme
+            user: this.props.location.state.user
         }, () => {console.log(this.state)});
     }
 
     render() {
         return (
             <Container>
-                <div>
-                    Curtis Yungen
+                <div
+                    className={`bg-${this.props.theme} contacts-bg-${this.props.theme}`}
+                >
+                    <ul>
+                        <li>Carson Wack</li>
+                        <li>Curtis Yungen</li>
+                        <li>Joseph Bizar</li>
+                        <li>Meri Arzumanyan</li>
+                        <li>Swapna Lia Anil</li>
+                        <li>Vahe Minasyan</li>
+                    </ul>
                 </div>
             </Container>
         );
