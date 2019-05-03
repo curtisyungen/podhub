0002e0831dedc013d40b2b1bb2c66f6e52658878
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-03 14:12:56 -0700

    Add files via upload

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -29,42 +29,42 @@ class Contact extends Component {
                         <img src="" />
                         <p>Carson Wack</p>
                         <p></p>
-                        <a href=""></a>
+                        <a href="" target="_blank"></a>
                     </div>
 
                     <div className="teamMember">
                         <img src="" />
                         <p>Curtis Yungen</p>
                         <p>curtisyungen@gmail.com</p>
-                        <a href="https://linkedin.com/in/curtisyungen">LinkedIn</a>
+                        <a href="https://linkedin.com/in/curtisyungen" target="_blank">LinkedIn</a>
                     </div>
 
                     <div className="teamMember">
                         <img src="" />
                         <p>Joseph Bizar</p>
                         <p></p>
-                        <a href=""></a>
+                        <a href="" target="_blank"></a>
                     </div>
 
                     <div className="teamMember">
                         <img src="" />
                         <p>Meri Arzumanyan</p>
                         <p></p>
-                        <a href=""></a>
+                        <a href="" target="_blank"></a>
                     </div>
 
                     <div className="teamMember">
                         <img src="" />
                         <p>Swapna Lia Anil</p>
                         <p></p>
-                        <a href=""></a>
+                        <a href="" target="_blank"></a>
                     </div>
 
                     <div className="teamMember">
                         <img src="" />
                         <p>Vahe Minasyan</p>
                         <p></p>
-                        <a href=""></a>
+                        <a href="" target="_blank"></a>
                     </div>
                 </div>
             </Container>
