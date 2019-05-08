83460475b896822b6abbd699aba28336ce13f1f9
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-08 09:01:01 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/public/index.html"), function(err) {
+router.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"), function(err) {
     if (err) {
       res.status(500).send(err);
     }
