461218bba160d9a930410e805441fd8036ca5895
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-29 13:43:42 -0700

    Add files via upload

--- client/src/pages/UserSearch.js
+++ client/src/pages/UserSearch.js
@@ -173,7 +173,7 @@ class UserSearch extends Component {
                                 />
                                 {user.follow ? (
                                     <button
-                                        className="btn btn-outline-light buttonPosition"
+                                        className={`btn btn-outline-${this.props.theme} buttonPosition`}
                                         onClick={(event) => {
                                             event.preventDefault();
                                             this.unFollowUser(user.id)
@@ -184,7 +184,7 @@ class UserSearch extends Component {
                                     </button>
                                 ) : (
                                         <button
-                                            className="btn btn-outline-light buttonPosition"
+                                            className={`btn btn-outline-${this.props.theme} buttonPosition`}
                                             onClick={(event) => {
                                                 event.preventDefault();
                                                 this.followUser(user.id)
